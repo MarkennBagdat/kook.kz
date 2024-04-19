1611b668a5a5070735722a54378e03a023b888dd
MarkennBagdat <bagdatmarkenn@gmail.com>
2024-04-20 04:59:26 +0500

    changes

--- src/pages/MainPage/Divider/Features.js
+++ src/pages/MainPage/Divider/Features.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./MainPage.css";
 import CheckFilled from "../../../Picture/check_filled.png";
 import Banner from "../../../Picture/Banner.png";
+import Banner2 from '../../../Picture/Banner (2).png';
 
 // Accept `openFormModal` as a prop
 const Features = ({ openFormModal }) => {
